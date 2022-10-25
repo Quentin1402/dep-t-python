d6810212710927b8e4c040793a10e214330e27f4
Quentin1402 <113422793+Quentin1402@users.noreply.github.com>
2022-10-25 16:15:25 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 class Personne(BaseModel):
     nom: str
@@ -13,9 +18,18 @@ class Personne(BaseModel):
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(personne: Personne):
-    personne.nom = "Marembert"
-    personne.prenom = "Quentin"
-    personne.age =  22
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.post("/personne/")
+def create_item(personne: Personne):
     return personne
